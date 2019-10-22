5271111e12b913cc9910300fc7375e86248b4519
Sushil <sushilrajbhar2019@gmail.com>
2019-10-22 14:15:13 +0530

    approver code

--- src/main/java/com/hcl/mediclaim/service/ClaimRequestService.java
+++ src/main/java/com/hcl/mediclaim/service/ClaimRequestService.java
@@ -1,7 +1,10 @@
 package com.hcl.mediclaim.service;
 
+import com.hcl.mediclaim.dto.ApproveClaimRequestDto;
+import com.hcl.mediclaim.dto.ApproveClaimResponseDto;
 import com.hcl.mediclaim.dto.MedicalClaimRequestDto;
 import com.hcl.mediclaim.dto.MedicalClaimResponseDto;
+import com.hcl.mediclaim.exception.InvalidClaimIdException;
 import com.hcl.mediclaim.exception.InvalidPolicyIdException;
 import com.hcl.mediclaim.exception.InvalidUserException;
 
@@ -13,5 +16,7 @@ import com.hcl.mediclaim.exception.InvalidUserException;
 public interface ClaimRequestService {
 	
 	public MedicalClaimResponseDto applyMedicalClaim(MedicalClaimRequestDto medicalClaimRequestDto) throws InvalidUserException, InvalidPolicyIdException;
+	
+	public ApproveClaimResponseDto approveMedicalClaim(int claimId,ApproveClaimRequestDto claimRequestDto) throws InvalidClaimIdException, InvalidUserException;
 
 }
